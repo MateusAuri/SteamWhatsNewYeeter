930a1acb6ea7a44ff76df57b00e426dd47717227
Mateus Auri <mateus.auri@gmail.com>
2026-06-06 02:33:23 -0300

    Update SteamWhatsNewYeeter.bat
    202606060230

--- SteamWhatsNewYeeter.bat
+++ SteamWhatsNewYeeter.bat
@@ -2,22 +2,27 @@
 :: 
 ::               MateusAuri's
 :: Steam Library 'What's New' Yeeter script
-::           Version 202312290855
+::
+::What's this?
+::    This batch/powershell script hides that annoying "What's New" shelf from your Steam Library (on Windows)
+::
+::Use instructions:
+::    - Navigate to your Steam install directory (usually "C:\Program Files (x86)\Steam")
+::    - Go into "steamui\css"
+::    - Drop both BAT and PS1 files inside this "css" folder (the files should be in the same folder as "chunk~2dcc5aaf7.css")
+::    - Double-click the BAT file
+::    - Repeat the previous step whenever "What's New" pops back again into your library
 :: 
-:: What's this?
-::     This batch/powershell script hides that annoying "What's New" shelf or section from your Steam Library
+::    You may want to create a shortcut to the BAT file for easier access in the future
 :: 
-:: Use instructions:
-::     - Exit Steam
-::     - Navigate to your Steam install directory (usually "C:\Program Files (x86)\Steam")
-::     - Go into "steamui\css"
-::     - Drop both BAT and PS1 files in the "css" folder (it should be in the same folder as "chunk~2dcc5aaf7.css")
-::     - Double-click this BAT file
-::     - Repeat the previous step whenever "What's New" pops back again into your library
-:: You may want to create a shortcut to the BAT file for easier access in the future
+:: This BAT file only exists to set the required permissions and then run the actual script from the PS1 file.
+:: Because nothing's ever simple with computers. I hate this.
 :: 
-:: Sets PowerShell permission to be able to run it at all
-:: Because nothing ever can be simple with computers I hate this
-:: And then runs the actual script file
 POWERSHELL -ExecutionPolicy Bypass ".\SteamWhatsNewYeeter.ps1"
-TIMEOUT 60
+ECHO 
+ECHO.
+ECHO This window will close itself in 90 seconds...
+ECHO Enjoy your uncluttered library! :)
+ECHO Bye! o/
+ECHO.
+TIMEOUT 90
